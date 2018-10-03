58e3ecf20e587b3d32860e2fd8f5cb6de60be03a
ajmoebius <ajmoebius@ecc-jaguar.csuchico.edu>
2018-10-03 00:50:08 -0700

    Make Me Shout

--- main.cpp
+++ main.cpp
@@ -7,9 +7,27 @@ using namespace std;
 int main()
 {
   string said;  
+  int index;
 
   cout<<"What do you say?\n";
   getline(cin,said);
+ 
+
+  for(int index=0; index < said.length(); index++)
+  {
+    char converted = said[index];
+    if (said[index]>='a' && said[index] <='z')
+    {
+      converted = said[index] -('a'-'A');
+    }
+    /*
+    else if(said[index]==".")
+    {
+      said[index]="!";
+    }
+    */
+    cout<<said[index]<<" "<<converted<<endl;
+  }
 
   cout<<"As a shout, you said:\n"<<said<<endl;
 
